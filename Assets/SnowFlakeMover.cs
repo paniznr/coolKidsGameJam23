6815ec7b3185e2621a6f7715659f235c380896a8
dqhntt <48117687+dqhntt@users.noreply.github.com>
2023-07-02 11:35:42 -0700

    Add snowflakes

--- Assets/SnowFlakeMover.cs
+++ Assets/SnowFlakeMover.cs
@@ -14,6 +14,9 @@ public class SnowFlakeMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = (transform.position - player.transform.position) * Time.deltaTime;
+
+        transform.position = new Vector3(0, (-6 + player.transform.position.y), 0);
+        Debug.Log("snow pos " + transform.position);
+        Debug.Log("player pos " + player.transform.position);
     }
 }
